d65078171e20e4b05d5afd0aca59683e5ccf64b8
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2024-04-03 08:16:45 -0300

    falta configurar app properties_dev

--- produto-mvc_aula_11/src/main/java/br/com/fiap/produtomvc/models/Categoria.java
+++ produto-mvc_aula_11/src/main/java/br/com/fiap/produtomvc/models/Categoria.java
@@ -1,10 +1,19 @@
 package br.com.fiap.produtomvc.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = {"nome"})
 
 @Entity
 @Table(name = "tb_categoria")
@@ -12,49 +21,19 @@ public class Categoria {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "categoria_seq", sequenceName = "categoria_seq", allocationSize = 1)
     private Long id;
+
+    @NotBlank(message = "Campo requerido")
+    @Size(min = 3, message = "O nome deve ter no mínimo 3 carateres")
+    @Column(length = 150, nullable = false)
     private String nome;
 
-    public Categoria() {}
+    @OneToMany(mappedBy = "categoria")
+    private List<Produto> produtos = new ArrayList<>();
 
     public Categoria(String nome) {
         this.nome = nome;
-    }
-
-    @OneToMany(mappedBy = "categoria")
-   private List<Produto> produtos = new ArrayList<>();
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<Produto> getProdutos() {
-        return produtos;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Categoria categoria = (Categoria) o;
-        return Objects.equals(id, categoria.id);
     }
 
     @Override
