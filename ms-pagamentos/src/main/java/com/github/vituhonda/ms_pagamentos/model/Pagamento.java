b9921347fd4ba6e109594633fb6c26109e12b38f
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2024-08-07 09:25:09 -0300

    validação atributos de pagamento

--- ms-pagamentos/src/main/java/com/github/vituhonda/ms_pagamentos/model/Pagamento.java
+++ ms-pagamentos/src/main/java/com/github/vituhonda/ms_pagamentos/model/Pagamento.java
@@ -1,5 +1,6 @@
 package com.github.vituhonda.ms_pagamentos.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,16 +12,25 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Setter
 @Getter
+@Entity
+@Table(name = "tb_pagamentos")
 public class Pagamento {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private BigDecimal valor;
     private String nome;
     private String numeroDoCartao;
     private String validade;
     private String codigoDeSeguranca;
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
     private Status status;
+    @Column(nullable = false)
     private Long pedidoId;
+    @Column(nullable = false)
     private Long formaDePagamentoId; // 1 - dinheiro | 2 - cartão | 3 - pix
 
 }
