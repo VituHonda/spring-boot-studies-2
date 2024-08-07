8b21b36afca3044a3b8caafc2bfdd01251ae9956
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2024-08-07 08:44:59 -0300

    resto controller loja find all e find by id

--- api-produto/src/main/java/br/com/fiap/produtomvc/controllers/LojaController.java
+++ api-produto/src/main/java/br/com/fiap/produtomvc/controllers/LojaController.java
@@ -1,9 +1,15 @@
 package br.com.fiap.produtomvc.controllers;
 
+import br.com.fiap.produtomvc.dto.LojaDTO;
 import br.com.fiap.produtomvc.services.LojaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/lojas")
@@ -12,6 +18,16 @@ public class LojaController {
     @Autowired
     private LojaService service;
 
-    
+    @GetMapping
+    public ResponseEntity<List<LojaDTO>> findAll(){
+        List<LojaDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<LojaDTO> findById(@PathVariable Long id){
+        LojaDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
 
 }
