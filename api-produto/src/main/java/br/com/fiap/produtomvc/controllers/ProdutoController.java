6ecd7c4204e7c0d82b27b40c031e6d134b205643
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2024-08-07 08:18:07 -0300

    rest controller produto post

--- api-produto/src/main/java/br/com/fiap/produtomvc/controllers/ProdutoController.java
+++ api-produto/src/main/java/br/com/fiap/produtomvc/controllers/ProdutoController.java
@@ -6,7 +6,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,7 +30,16 @@ public class ProdutoController {
         return ResponseEntity.ok(dto);
     }
 
-    
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> insert(@RequestBody @Valid ProdutoDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
 
 
 }
