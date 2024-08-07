23606c8324f2365649a47e72a76ac53314948af9
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2024-08-07 08:06:26 -0300

    rest controller get produto

--- api-produto/src/main/java/br/com/fiap/produtomvc/controllers/ProdutoController.java
+++ api-produto/src/main/java/br/com/fiap/produtomvc/controllers/ProdutoController.java
@@ -1,9 +1,13 @@
 package br.com.fiap.produtomvc.controllers;
 
+import br.com.fiap.produtomvc.dto.ProdutoDTO;
 import br.com.fiap.produtomvc.services.ProdutoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -11,5 +15,14 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService service;
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoDTO>> findall() {
+        List<ProdutoDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
+    
+
 
 }
