0439193c97414302776451111cb5ff5ab1ab3fcb
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2024-08-07 08:53:37 -0300

    rest controller loja insert

--- api-produto/src/main/java/br/com/fiap/produtomvc/controllers/LojaController.java
+++ api-produto/src/main/java/br/com/fiap/produtomvc/controllers/LojaController.java
@@ -2,13 +2,13 @@ package br.com.fiap.produtomvc.controllers;
 
 import br.com.fiap.produtomvc.dto.LojaDTO;
 import br.com.fiap.produtomvc.services.LojaService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +28,17 @@ public class LojaController {
     public ResponseEntity<LojaDTO> findById(@PathVariable Long id){
         LojaDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<LojaDTO> insert(@RequestBody @Valid LojaDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
