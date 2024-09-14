284e8ec852026052d9416d5562735a5410b6385c
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2024-09-13 22:27:27 -0300

    produto service

--- ecommerce/src/main/java/br/com/fiap/ecommerce/service/ProdutoService.java
+++ ecommerce/src/main/java/br/com/fiap/ecommerce/service/ProdutoService.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
+
 
     @Autowired
     private ProdutoRepository produtoRepository;
@@ -17,4 +19,24 @@ public class ProdutoService {
         return produtoRepository.findAll();
     }
 
+    public Produto save(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+   public Produto update(Long id, Produto produto) {
+        return null;
+   }
+
+    public void delete(Long id) {
+        produtoRepository.deleteById(id);
+    }
+
+    public Optional<Produto> findById(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+
+    public boolean existsById(Long id) {
+        return produtoRepository.existsById(id);
+    }
 }
