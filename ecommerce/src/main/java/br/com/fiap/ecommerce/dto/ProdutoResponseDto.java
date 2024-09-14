7753e4373a563039904fb0a78bc29b6378ebd88e
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2024-09-13 21:03:30 -0300

    produto response dto

---
+++ ecommerce/src/main/java/br/com/fiap/ecommerce/dto/ProdutoResponseDto.java
@@ -0,0 +1,11 @@
+package br.com.fiap.ecommerce.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoResponseDto {
+    private Long id;
+    private String nome;
+}
