c7117bfe5aa8aa0e4be6bbeeb998ab84d4df2748
Vitor Tanabe <94921541+VituHonda@users.noreply.github.com>
2024-09-13 21:03:06 -0300

    simples produto controller

--- ecommerce/src/main/java/br/com/fiap/ecommerce/controller/ProdutoController.java
+++ ecommerce/src/main/java/br/com/fiap/ecommerce/controller/ProdutoController.java
@@ -1,19 +1,47 @@
 package br.com.fiap.ecommerce.controller;
 
+import br.com.fiap.ecommerce.dto.ProdutoRequestCreateDto;
+import br.com.fiap.ecommerce.dto.ProdutoResponseDto;
 import br.com.fiap.ecommerce.model.Produto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.fiap.ecommerce.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    @Autowired
+    private ProdutoService produtoService;
+
     @GetMapping
-    public List<Produto> list() {
+    public List<ProdutoResponseDto> list() {
+        List<Produto> produtos = produtoService.list();
         return null;
     }
+
+    @PostMapping
+    public ProdutoResponseDto create(ProdutoRequestCreateDto dto) {
+        return null;
+    }
+
+    @GetMapping("/id")
+    public Produto findById() {
+        return null;
+    }
+
+    @PutMapping("/id")
+    public void update() {
+
+    }
+
+    @DeleteMapping("/id")
+    public void delete() {
+
+    }
+
 
 }
